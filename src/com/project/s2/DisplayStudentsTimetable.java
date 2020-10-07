a28fdcd8a56bccb7b6d37305c32db741fed024aa
SithijaBimsara <sithijabimsara@gmail.com>
2020-10-07 20:02:57 +0530

    bug fixed

--- src/com/project/s2/DisplayStudentsTimetable.java
+++ src/com/project/s2/DisplayStudentsTimetable.java
@@ -247,7 +247,9 @@ public class DisplayStudentsTimetable extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    //main method
     public static void main(String args[]) {
+       
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
