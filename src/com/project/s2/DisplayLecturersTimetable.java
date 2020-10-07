a7387628ed599ad383b2ba4307a8c987c57c213d
SithijaBimsara <sithijabimsara@gmail.com>
2020-10-07 19:56:41 +0530

    Changed buttons in ViewLectures

--- src/com/project/s2/DisplayLecturersTimetable.java
+++ src/com/project/s2/DisplayLecturersTimetable.java
@@ -162,7 +162,7 @@ public class DisplayLecturersTimetable extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
-        
+        //new method
         int lid=Integer.parseInt(r_id.getText());
         getLectuerName(lid);
         DefaultTableModel model1 =(DefaultTableModel)roomstructure_tab.getModel();
